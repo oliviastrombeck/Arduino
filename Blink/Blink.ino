38cd02daa63d0d9fb5adcff4a0fe30391785c985
Strombeck <02olivia.strombeck@gulllakecs.org>
2025-04-21 14:05:04 -0400

    Updated Code with whiteLED = 12

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -22,16 +22,19 @@
   https://docs.arduino.cc/built-in-examples/basics/Blink/
 */
 
+
+int whiteLED = 12;
+
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin 12 as an output.
-  pinMode(12, OUTPUT);
+  pinMode(whiteLED, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(12, HIGH);  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(whiteLED, HIGH);  // turn the LED on (HIGH is the voltage level)
   delay(3000);                      // wait for a second
-  digitalWrite(12, LOW);   // turn the LED off by making the voltage LOW
+  digitalWrite(whiteLED, LOW);   // turn the LED off by making the voltage LOW
   delay(500);                      // wait for a second
 }
